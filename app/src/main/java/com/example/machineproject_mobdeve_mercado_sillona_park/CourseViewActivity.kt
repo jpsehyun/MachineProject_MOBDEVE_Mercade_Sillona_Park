32e32cb193145eece30a357f8c3c76ab05f377ad
BruceBM <bruce_mercado@dlsu.edu.ph>
2023-10-18 13:31:38 +0800

    Course View Navigation

--- app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/CourseViewActivity.kt
+++ app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/CourseViewActivity.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.IllegalArgumentException
 
 class CourseViewActivity: AppCompatActivity() {
     private lateinit var addCourseButton: Button
     private lateinit var scheduleButton: Button
+    private lateinit var homeButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,16 +22,28 @@ class CourseViewActivity: AppCompatActivity() {
     fun initializeUI() {
         addCourseButton = findViewById(R.id.CV_AddCourse_Btn)
         scheduleButton = findViewById(R.id.CV_Schedule_Btn)
+        homeButton = findViewById(R.id.CV_Home_Btn)
     }
 
     fun initializeListener() {
         addCourseButton.setOnClickListener {
-            loadAddCourse()
+            loadNextScreen("courseEdit")
+        }
+        scheduleButton.setOnClickListener{
+            loadNextScreen("schedule")
+        }
+        homeButton.setOnClickListener{
+            loadNextScreen("home")
         }
     }
 
-    fun loadAddCourse(){
-        val intentLoadNextActivity = Intent(this, AddEditViewActivity::class.java)
+    private fun loadNextScreen(destination: String) {
+        var intentLoadNextActivity = when (destination) {
+            "courseEdit" -> Intent(this, AddEditViewActivity::class.java)
+            "schedule" -> Intent(this, MondayActivity::class.java)
+            "home" -> Intent(this, MainActivity::class.java)
+            else -> throw IllegalArgumentException("Invalid destination: $destination")
+        }
         startActivity(intentLoadNextActivity)
     }
 }
