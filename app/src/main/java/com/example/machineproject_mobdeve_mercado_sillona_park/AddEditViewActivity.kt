e5a3ac262b7c02794f7a9362cc71e694a74d0600
Eugene Sillona <93899090+jojiSillona@users.noreply.github.com>
2023-10-21 19:32:03 +0800

    NEW BRANCH CREATED: add_edit_view ======================== INTERNAL FILES: strings.xml - added string array for days
    KOTLIN FILES:
    AddEditViewActivity:
    - modified spinner
    
    LAYOUTS:
    add_edit_view.xml:
    - refactored spinner to "daySpinner"

--- app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/AddEditViewActivity.kt
+++ app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/AddEditViewActivity.kt
@@ -1,11 +1,28 @@
 package com.example.machineproject_mobdeve_mercado_sillona_park
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class AddEditViewActivity: AppCompatActivity() {
+    private lateinit var daySpinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_edit_view)
+
+        initializeUI()
+    }
+
+    fun initializeUI(){
+        this.daySpinner = findViewById(R.id.daySpinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.days_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            daySpinner.adapter = adapter
+        }
     }
 }
